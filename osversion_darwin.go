d6f9971f8ccfe77f5c5d4101b4040cf698f0ef27
Ox Cart <ox.to.a.cart@gmail.com>
2023-02-21 06:04:31 -0600

    Added support for newer macOS versions

--- osversion_darwin.go
+++ osversion_darwin.go
@@ -38,30 +38,38 @@ func GetHumanReadable() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	if version.Major < 4 || version.Major > 18 {
+	if version.Major < 4 || version.Major > 22 {
 		return fmt.Sprintf("Unknown OS X version: %s", version.String()), nil
 	}
 
+	decimal := strconv.FormatUint(version.Patch, 10)
+	if version.Major >= 18 {
+		decimal = strconv.FormatUint(version.Minor-1, 10)
+	}
 	return strings.Replace(versions[version.Major-4],
-		"{patch}",
-		strconv.FormatUint(version.Patch, 10),
+		"{decimal}",
+		decimal,
 		1), nil
 }
 
 var versions = []string{
-	"OS X 10.0.{patch} Cheetah",
-	"OS X 10.1.{patch} Puma",
-	"OS X 10.2.{patch} Jaguar",
-	"OS X 10.3.{patch} Panther",
-	"OS X 10.4.{patch} Tiger",
-	"OS X 10.5.{patch} Leopard",
-	"OS X 10.6.{patch} Snow Leopard",
-	"OS X 10.7.{patch} Lion",
-	"OS X 10.8.{patch} Mountain Lion",
-	"OS X 10.9.{patch} Mavericks",
-	"OS X 10.10.{patch} Yosemite",
-	"OS X 10.11.{patch} El Capitan",
-	"macOS 10.12.{patch} Sierra",
-	"macOS 10.13.{patch} High Sierra",
-	"macOS 10.14.{patch} Mojave",
+	"OS X 10.0.{decimal} Cheetah",
+	"OS X 10.1.{decimal} Puma",
+	"OS X 10.2.{decimal} Jaguar",
+	"OS X 10.3.{decimal} Panther",
+	"OS X 10.4.{decimal} Tiger",
+	"OS X 10.5.{decimal} Leopard",
+	"OS X 10.6.{decimal} Snow Leopard",
+	"OS X 10.7.{decimal} Lion",
+	"OS X 10.8.{decimal} Mountain Lion",
+	"OS X 10.9.{decimal} Mavericks",
+	"OS X 10.10.{decimal} Yosemite",
+	"OS X 10.11.{decimal} El Capitan",
+	"macOS 10.12.{decimal} Sierra",
+	"macOS 10.13.{decimal} High Sierra",
+	"macOS 10.14.{decimal} Mojave",
+	"macOS 10.15.{decimal} Catalina",
+	"macOS 11.{decimal} Big Sur",
+	"macOS 12.{decimal} Monterey",
+	"macOS 13.{decimal} Ventura",
 }
